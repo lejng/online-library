b2e2dc493c95e91a26c1555a9c61046c6624fecc
leonid13171996 <leonid13171996@outlook.com>
2018-03-10 03:38:43 +0300

    add postgres

--- src/java/net/category/model/Book.java
+++ src/java/net/category/model/Book.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class Book  {
     @Id
     @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
     @Column(name = "name")
