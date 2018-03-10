b76c14902e71f9cc2b9b3f11c8f16c8d2c3eebdb
leonid13171996 <leonid13171996@outlook.com>
2018-03-10 14:08:04 +0300

    add config for deploy on heroku

--- src/java/net/category/service/user/UserDetailsServiceImpl.java
+++ src/java/net/category/service/user/UserDetailsServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashSet;
@@ -17,6 +18,7 @@ import java.util.Set;
 /**
  * Implementation of {@link org.springframework.security.core.userdetails.UserDetailsService}
  */
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService{
 
     @Autowired
